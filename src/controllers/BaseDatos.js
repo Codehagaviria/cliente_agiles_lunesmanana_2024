4edc4a00604a0dfbcb6d94d80dbfd847c27747cb
Pablo Rivera <hgaviria@cesde.net>
2024-09-20 07:24:25 -0500

    se crea archivo para simular la BD

---
+++ src/controllers/BaseDatos.js
@@ -0,0 +1,16 @@
+let gastosDos=Array(
+   {
+        id:1,
+        descripcion:"Pasaje de sabaneta a cesde",
+        monto:8000,
+        fecha:"2024-09-20"
+
+    },
+    {
+        id:2,
+        descripcion:"Empanada de pollo + coca cola",
+        monto:10000,
+        fecha:"2024-09-20"
+    
+    }
+)
